ba55a6b898432a988138414654525f2d9e853ec1
Arslonbek25 <amadyarov25@gmail.com>
2023-08-07 20:22:48 +0500

    Add feature to maintain non-encoded chars during URL re-encoding

--- jsonurl.py
+++ jsonurl.py
@@ -1,4 +1,20 @@
+import json
+import string
 import urllib.parse
+
+
+def get_encoded_chars(url):
+    return "".join(
+        list(dict.fromkeys([char for char in url if char in string.punctuation]))
+    ).replace("%", "")
+
+
+def is_valid_json(data):
+    try:
+        json.loads(data)
+        return True
+    except ValueError:
+        return False
 
 
 def decode_str(data):
@@ -9,16 +25,26 @@ def decode_dict(d):
     if isinstance(d, dict):
         return {urllib.parse.unquote(k): decode_dict(v) for k, v in d.items()}
     elif isinstance(d, str):
-        pairs = dict(urllib.parse.parse_qsl(d))
-        if "&" in pairs and "=" in pairs:
-            return {k: urllib.parse.unquote(v) for k, v in pairs.items()}
-        else:
-            return pairs
+        # pairs = dict(urllib.parse.parse_qsl(d))
+        # print(pairs)
+        # if "&" in pairs and "=" in pairs:
+        #     return {k: urllib.parse.unquote(v) for k, v in pairs.items()}
+        # else:
+        return urllib.parse.unquote(d)
     elif isinstance(d, list):
         return [decode_dict(v) for v in d]
     else:
         return d
 
 
-def encode(data):
-    return urllib.parse.quote(data)
+def encode(data, safe):
+    return urllib.parse.quote(data, safe=safe + " \n")
+
+
+if __name__ == "__main__":
+    t = encode(
+        "https://www.google.com/search?q=python+url+encoding&oq=python+url+encoding&aqs=chrome..69i57j0l7.2873j0j7&sourceid=chrome&ie=UTF-8",
+        safe="",
+    )
+
+    print(t)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from jsonurl import decode_dict, decode_str, encode
+from jsonurl import decode_dict, decode_str, encode, get_encoded_chars, is_valid_json
 
 e_dir = "encoded"
 d_dir = "decoded"
@@ -24,28 +24,41 @@ def write_file(path, content):
 def decode_files():
     for file in e_files:
         data = read_file(os.path.join(e_dir, file))
-        if data.startswith("{") and ":" in data:
+
+        if is_valid_json(data):
             data_dict = json.loads(data)
+            # print(file)
             data = decode_dict(data_dict)
+            # print(data)
         else:
             data = decode_str(data)
 
         if isinstance(data, dict):
             data = json.dumps(data, indent=4)
 
-        ext = "json" if data.startswith("{") and data.endswith("}") else "txt"
+        ext = "json" if is_valid_json(data) else "txt"
         fn = os.path.splitext(file)[0]
+        print(fn, ext)
         write_file(os.path.join(d_dir, f"{fn}.{ext}"), data)
 
 
 def encode_files():
     for file in d_files:
         data = read_file(os.path.join(d_dir, file))
-        data = encode(data)
         fn = os.path.splitext(file)[0]
-        write_file(os.path.join(e_dir, f"{fn}.txt"), data)
+        e_file = os.path.join(e_dir, f"{fn}.txt")
+        decoded_data = read_file(e_file)
+        safe = get_encoded_chars(decoded_data)
+        print(fn, f"Z{safe}Z")
+        data = encode(data, safe)
+        
+        write_file(e_file, data)
 
 
 # Uncomment the function you want to run
 # decode_files()
-# encode_files()
+encode_files()
+
+# with open("encoded/curl4.txt", "r") as file:
+#     data = file.read()
+#     print(get_encoded_chars(data))
